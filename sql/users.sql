17e4809483277db06fbe302d0e083094b218af67
Kai Hendry <hendry@iki.fi>
2019-03-31 08:57:24 +0800

    Getting  "error": "Error 1366: Incorrect string value: '\\xF0\\x9F\\x98\\x8B' for column `rest_api_example`.`users`.`name` at row 1"
    When posting an emoji

--- sql/users.sql
+++ sql/users.sql
@@ -1,3 +1,5 @@
+SET NAMES utf8mb4; /* https://stackoverflow.com/a/35189650/4534 */
+/* Above doesn't appear to work https://s.natalian.org/2019-03-31/1553993788_2560x1440.png */
 CREATE DATABASE rest_api_example;
 USE rest_api_example;
 CREATE TABLE users (
